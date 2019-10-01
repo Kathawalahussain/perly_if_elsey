1168ee8ac401129ab74a956ff6a30a1415c069e8
ksb1997 <44491299+ksb1997@users.noreply.github.com>
2019-10-02 00:47:15 +0530

    Create perly_ifelse_elsey.pl

---
+++ perly_ifelse_elsey.pl
@@ -0,0 +1,16 @@
+$a = 1;
+$b = 2;
+ 
+if($a == $b)
+{
+   print("a and b are equal\n");
+}elsif($a > $b)
+{
+   print("a is greater than b\n");
+}else
+{
+   print("a is less than b\n");
+}
+
+
+#greater than less than
